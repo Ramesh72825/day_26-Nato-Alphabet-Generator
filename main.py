4b8c078c7f0964063e78c4cd2256e0ebd53d8975
अध्येता <63831786+Ramesh72825@users.noreply.github.com>
2023-08-02 12:33:08 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
-#For Loop
-numbers = [1, 2, 3]
-new_list = []
-for n in numbers:
-    add_1 = n + 1
-    new_list.append(add_1)
-
-#List Comprehension
-new_list = [n + 1 for n in numbers]
-
-#String as List
-name = "Angela"
-letters_list = [letter for letter in name]
-
-#Range as List
-range_list = [n * 2 for n in range(1, 5)]
-
-#Conditional List Comprenhension
-names = ["Alex", "Beth", "Caroline", "Dave", "Elanor", "Freddie"]
-short_names = [name for name in names if len(name) < 5]
-
-upper_case_names = [name.upper() for name in names if len(name) > 4]
-
-#Dictionary Comprehension
-import random
-student_grades = {name: random.randint(1, 100) for name in names}
-print(student_grades)
-
-passed_students = {
-    student: grade
-    for (student, grade) in student_grades.items() if grade >= 60
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
 }
-print(passed_students)
+
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
+
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+
+#Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    #Access index and row
+    #Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+name_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+# print(name_dict)
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+output_list = [name_dict[letter] for letter in word]
+print(output_list)
